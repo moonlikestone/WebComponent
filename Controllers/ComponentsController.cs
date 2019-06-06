ba43efb150e9ad87670ca0dd38d79f0fb7e78b03
Jing <moonlikestone@gmail.com>
2019-06-06 14:04:01 +0800

    Add Controllers
    補充完整的控制器

---
+++ Controllers/ComponentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebComponent.Models;
+
+namespace WebComponent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentsController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public ComponentsController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Components
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Component>>> GetComponent()
+        {
+            return await _context.Component.ToListAsync();
+        }
+
+        // GET: api/Components/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Component>> GetComponent(long id)
+        {
+            var component = await _context.Component.FindAsync(id);
+
+            if (component == null)
+            {
+                return NotFound();
+            }
+
+            return component;
+        }
+
+        // PUT: api/Components/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComponent(long id, Component component)
+        {
+            if (id != component.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(component).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComponentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Components
+        [HttpPost]
+        public async Task<ActionResult<Component>> PostComponent(Component component)
+        {
+            _context.Component.Add(component);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ComponentExists(component.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetComponent", new { id = component.Id }, component);
+        }
+
+        // DELETE: api/Components/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Component>> DeleteComponent(long id)
+        {
+            var component = await _context.Component.FindAsync(id);
+            if (component == null)
+            {
+                return NotFound();
+            }
+
+            _context.Component.Remove(component);
+            await _context.SaveChangesAsync();
+
+            return component;
+        }
+
+        private bool ComponentExists(long id)
+        {
+            return _context.Component.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/DownloadRecordsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebComponent.Models;
+
+namespace WebComponent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadRecordsController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public DownloadRecordsController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DownloadRecords
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DownloadRecord>>> GetDownloadRecord()
+        {
+            return await _context.DownloadRecord.ToListAsync();
+        }
+
+        // GET: api/DownloadRecords/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DownloadRecord>> GetDownloadRecord(long id)
+        {
+            var downloadRecord = await _context.DownloadRecord.FindAsync(id);
+
+            if (downloadRecord == null)
+            {
+                return NotFound();
+            }
+
+            return downloadRecord;
+        }
+
+        // PUT: api/DownloadRecords/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDownloadRecord(long id, DownloadRecord downloadRecord)
+        {
+            if (id != downloadRecord.RecordId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(downloadRecord).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DownloadRecordExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DownloadRecords
+        [HttpPost]
+        public async Task<ActionResult<DownloadRecord>> PostDownloadRecord(DownloadRecord downloadRecord)
+        {
+            _context.DownloadRecord.Add(downloadRecord);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DownloadRecordExists(downloadRecord.RecordId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDownloadRecord", new { id = downloadRecord.RecordId }, downloadRecord);
+        }
+
+        // DELETE: api/DownloadRecords/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DownloadRecord>> DeleteDownloadRecord(long id)
+        {
+            var downloadRecord = await _context.DownloadRecord.FindAsync(id);
+            if (downloadRecord == null)
+            {
+                return NotFound();
+            }
+
+            _context.DownloadRecord.Remove(downloadRecord);
+            await _context.SaveChangesAsync();
+
+            return downloadRecord;
+        }
+
+        private bool DownloadRecordExists(long id)
+        {
+            return _context.DownloadRecord.Any(e => e.RecordId == id);
+        }
+    }
+}
